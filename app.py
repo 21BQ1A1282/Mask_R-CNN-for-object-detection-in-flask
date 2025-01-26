ae36a53ea0db8be3305d871db649895b5786769a
Sai Manikanta Madhavarapu <madhavarapu.saimanikanta@gmail.com>
2025-01-26 11:21:15 +0530

    files added

--- app.py
+++ app.py
@@ -657,5 +657,5 @@ def save_users(users):
 
 
 # Run the app
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=8000, debug=True)
